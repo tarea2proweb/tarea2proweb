0f9d85552621366c3e57697715ba7b34e6df7153
tarea2proweb <tarea2.proweb@gmail.com>
2021-05-27 19:41:40 -0500

    Cambio del Index

---
+++ js/init.js
@@ -0,0 +1,9 @@
+firebase.initializeApp ({
+    apiKey: "AIzaSyDnlcST0wpbJhGX31uUnoUiMVPdT4FX0-w",
+    authDomain: "tareaproweb.firebaseapp.com",
+    projectId: "tareaproweb",
+    storageBucket: "tareaproweb.appspot.com",
+    messagingSenderId: "331029879471",
+    appId: "1:331029879471:web:cde296eb7a8d01ca3b525a",
+    measurementId: "G-TK21EP1N8X"
+  });
